7ae2552a92de247713bc75855ee190b210613553
Raja-s-k <rajask42042@gmail.com>
2022-07-05 18:16:14 +0530

    Home automation system

--- S2_ESL_project/S2_ESL_project.ino
+++ S2_ESL_project/S2_ESL_project.ino
@@ -9,59 +9,102 @@ char bluetooth;
 
 void setup()
 {
-  myservo.attach(2);
+  myservo.attach(3);
   BTSerial.begin(9600);
   Serial.begin(9600);
+  pinMode(A2, INPUT);
   pinMode(13,OUTPUT);//Yellow Light
-  pinMode(12,OUTPUT);//Green light
-  pinMode(11,OUTPUT);
+  pinMode(12,OUTPUT);//red light
 }
 
 void loop()
 {
   LDR=map(analogRead(A1),0,1023,0,255);
-  if (BTSerial.listen()>0 && LDR <=50)//where value == the value we get in adjusting LDR
+  if (BTSerial.isListening()>0)//where value == the value we get in adjusting LDR
   {
-    int count=0;
-    if (count==0){
-    Serial.println("WELCOME.....THE LIGHTS ARE TURNED ON");
-    BTSerial.write("WELCOME.....THE LIGHTS ARE TURNED ON");
-    count+=1;}
-    digitalWrite(13,HIGH);//Yellow light
-    digitalWrite(12,HIGH);//Red light
-    //digitalWrite(11,HIGH); 
-    if (BTSerial.available()>0){
-      bluetooth=BTSerial.read();
-      if (bluetooth=='o')
+// -------------------- door rotation -----------------------
+    if( LDR <= 100){
+      bluetooth = BTSerial.read();
+      digitalWrite(12, HIGH);
+      digitalWrite(13, HIGH);
+      if(BTSerial.available()>=0 ){
+        if (bluetooth=='o')
       {
-        for(int rotate=0;rotate<=180;rotate++)
-        {
-          myservo.write(rotate);
+        for(int i = 0; i < 180; i++){
+          myservo.write(180);
         }
         Serial.println("The Door is Opened");
         BTSerial.println("The Door is Opened");
       }
-      else if(bluetooth=='f')
+      else if(bluetooth=='c')
       {
-        for (int rotate=180;rotate>=0;rotate--)
-        {
-          myservo.write(rotate);
-        }
+        myservo.write(0);
         Serial.println("The Door is closed");
         BTSerial.println("The Door is closed");
       }
-      else if (bluetooth=='O')
-      {
-        digitalWrite(12,HIGH);
-        digitalWrite(13,LOW);
-        printf("The Red Light is on");
+      if (bluetooth=='W'){
+      myservo.write(0);
+      Serial.println("Intruder mode! calling the authorities.");
+      BTSerial.write("Intruder mode! calling the authorities.");
+      digitalWrite(12, LOW);
+      digitalWrite(13, LOW);
+      delay(100);
+      digitalWrite(12, HIGH);
+      digitalWrite(13, HIGH);
+      delay(100);
+    }
       }
-      else if(bluetooth=='F')
+      delay(1000);
+    }
+    else if(LDR >= 100){
+      bluetooth = BTSerial.read();
+      if(BTSerial.available()>= 0)
       {
-        digitalWrite(13,HIGH);
-        digitalWrite(12,LOW);
-        BTSerial.println("The Yellow Light is on");
-        Serial.println("The Yellow light is on:");
+        if(bluetooth=='W'){
+      myservo.write(0);
+      Serial.println("Intruder mode! calling the authorities.");
+      BTSerial.write("Intruder mode! calling the authorities.");
+      digitalWrite(12, LOW);
+      digitalWrite(13, LOW);
+      delay(100);
+      digitalWrite(12, HIGH);
+      digitalWrite(13, HIGH);
+      delay(100);
+    }
+      if (bluetooth=='o')
+      {
+        myservo.write(180);
+        Serial.println("The Door is Opened");
+        BTSerial.println("The Door is Opened");
       }
+      else if(bluetooth=='c')
+      {
+        myservo.write(0);
+        Serial.println("The Door is closed");
+        BTSerial.println("The Door is closed");
+      }
+      if(bluetooth == 'F'){
+        digitalWrite(12, LOW);
+        Serial.println("Lights are OFF");
+        BTSerial.write("Lights are OFF");
+      }
+      else if(bluetooth == 'O'){
+        digitalWrite(12, HIGH);
+        Serial.println("Lights are ON");
+        BTSerial.write("Lights are ON");
+      }
+      if(bluetooth == 'Y'){
+        digitalWrite(13, HIGH);
+        Serial.println("Lights are ON");
+        BTSerial.write("Lights are ON");
+      }
+      else if(bluetooth == 'y'){
+        digitalWrite(13, LOW);
+        Serial.println("Lights are OFF");
+        BTSerial.write("Lights are OFF");
+      }
+      }
+      delay(1000);
+    }
   }
 }
